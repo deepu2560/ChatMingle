2abbc1069443d04c40bb60317876f554df1df634
unknown <deepanshugulia256@gmail.com>
2023-04-29 16:56:37 +0530

    learning socket.io

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,38 @@
 const express = require("express");
+const cors = require("cors");
+const { Server } = require("socket.io");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.listen(port, async () => {
+app.use(cors());
+
+const server = app.listen(port, async () => {
   try {
     console.log("===>>> Server started successfully on port:", port);
   } catch (error) {
     console.log("===>>> Server Starting error:", error.message);
+  }
+});
+const io = new Server(server, { cors: { origin: "*" } });
+
+io.on("connection", (socket) => {
+  console.log("Connected");
+
+  socket.on("newMessage", (data) => {
+    console.log(data);
+  });
+});
+
+io.on("disconnect", (socket) => {
+  console.log("disconnected");
+});
+
+app.get("/", async (req, res) => {
+  try {
+    res.send("Hello World");
+  } catch (error) {
+    console.log("==>> Home route server error.");
   }
 });
