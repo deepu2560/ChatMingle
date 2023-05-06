23e629717ca4317d5328f38fb32415f4ec36f366
unknown <deepanshugulia256@gmail.com>
2023-05-06 15:07:42 +0530

    group section done

--- backend/src/Models/Group_Models.js
+++ backend/src/Models/Group_Models.js
@@ -2,6 +2,16 @@ const mongoose = require("mongoose");
 
 const groupSchema = new mongoose.Schema(
   {
+    isRequest: { type: Boolean, required: false, default: false },
+    request: {
+      user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+        required: false,
+      },
+      title: { type: String, required: false },
+      content: { type: String, require: false },
+    },
     group_name: { type: Boolean, required: true },
     bio: { type: String, require: true },
   },
